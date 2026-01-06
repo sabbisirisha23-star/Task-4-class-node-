016fed0f9a3a3e446baa856cab6caf4446a6ca85
sabbisirisha23-star <sabbisirisha23@gmail.com>
2026-01-06 12:31:49 +0530

    Node class task 4

---
+++ Task 4.java
@@ -0,0 +1,44 @@
+
+class Node{
+    int data;
+    Node next;
+    
+    //constructor
+    Node(int a){
+        data=a;
+        next=null;
+    }
+    
+    //getdata
+    public void getdata(){
+       System.out.println(data); 
+    }
+    
+    //method getData
+    public void getData(){
+        System.out.println(data); 
+    }
+    //method getref
+     public void getRef(){
+        System.out.println(next); 
+    }
+    
+}
+class Main {
+    public static void main(String[] args) {
+        System.out.println("lakshmi");
+        Node a=new Node(50);
+        Node b=new Node(100);
+         System.out.println(a);
+         System.out.println(b);
+         a.getdata();
+         b.getdata();
+         
+         a.getRef();
+         a.next=b;
+         a.getRef();
+         
+         
+       
+    }
+}
